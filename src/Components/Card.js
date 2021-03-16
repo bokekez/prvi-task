0b1260725bf84ab626958a869392b3665ba9b7a0
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-16 18:42:15 +0100

    api dohvat

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,18 +2,40 @@ import React, {useState, useEffect} from 'react';
 
 const Card = () => {
     const [item, setItem] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch(`https://randomuser.me/api/`)
+        fetch(`https://randomuser.me/api/?offset=-1:00,0:00,+1:00&results=15&age>18`)
         .then(response => response.json())
-        .then(json => setItem(json))
-
+        .then(json => {
+            const tempItems = json.results.map(user => ({
+                'picture': user.picture,
+                'title': user.name.title,
+                'first': user.name.first,
+                'last': user.name.last,           
+            }))
+            setUsers([...users, ...tempItems])
+        })
     }, [])
 
+    // const getUser = () => {
+    //     console.log(item)
+    //     const tempItems = item.map(user => ( {
+    //         'picture': user.picture,
+    //         'title': user.name.title,
+    //         'first': user.name.first,
+    //         'last': user.name.last,           
+    //     }))
+    //     setUsers([...users, tempItems])
+    //     console.log(users)
+    // }
+    console.log(users)
     console.log(item)
 
     return (
-        <div></div>
+        <div>
+            {/* <button onClick={getUser}></button> */}
+        </div>
     )
 }
 
