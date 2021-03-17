57804a179601502e571071d5c577767d003052ff
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-17 23:58:23 +0100

    route test

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
         <Route exact={true} path="/" render={() =>(
           <Card load={load} setLoad={setLoad}/>
         )}/>
+        <Route exact={true} path="/home" to="/" />
         <Route exact={true} path="/Profile/:id" render={() => (
           <Profile/>
         )}/>
